dae467d2a61ef5f3f289c620dcc43cb699a063b7
Collin Jilbert <cjilbert504@gmail.com>
2020-06-03 11:36:39 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects) {
+  let win = arrayOfObjects.find(function(season) {return season.result === "W"});
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+}
