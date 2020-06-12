4eba8ad229bf0c9429109efe560074f855cfdb34
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 04:54:34 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const arrayOfObjects = [      // Brackets = ARRAY PART
 
 function superbowlWin(arg1){ // arg1 can be ARRAY OF OBJECTS
   let x = arg1.map( record => // this is like Ruby's  do |item| item.action
-    record   ) 
+    record.find( el =>
+      el.result === "W")
+    
+    
+    ) 
   
   
   
