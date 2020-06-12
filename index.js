d931422952ba62148aa70c9e95b33f086f73def0
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 04:26:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+/*
+Write a function called superbowlWin() that:
+  Receives 1 argument, an Array of JavaScript Objects
+  
+  Each object has two properties: year and result
+  
+  Use find() to test each Object to see if the result is "W" — a win!
+  
+  superbowlWin() should return the year when the win occurred (if it occurred at all!). If no
+  
+  win is found, it should return, sadly, undefined
+
+*/
 const testVar = {}
 
 function testFunc() {
   return "hi"
 }
+
+
