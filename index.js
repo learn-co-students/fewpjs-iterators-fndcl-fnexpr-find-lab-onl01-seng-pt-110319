ae9bee123255022dc4c52f1163eb9bab3542bd9d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-20 23:53:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(games) {
+  let win = games.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
