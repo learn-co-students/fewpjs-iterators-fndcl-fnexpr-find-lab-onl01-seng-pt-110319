014c82ef2a3d942417bf3919445fdc864a71ce02
kchohee <kchohee0630@gmail.com>
2020-06-28 13:08:10 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record){
+  let win = record.find(record=> record.result === "W")
+  return win ? win.year : undefined
+}
