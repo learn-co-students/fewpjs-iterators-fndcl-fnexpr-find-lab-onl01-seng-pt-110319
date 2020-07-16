ce1d05407ba03004d0c8f2b7d347419e53620b22
nicksolony <nicksolony@gmail.com>
2020-07-15 20:44:19 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+
+function superbowlWin(record) {
+  let won_year = record.find(function(object){
+    //console.log(object);
+    //console.log(object.result);
+    return object.result==="W"
+    //return object === {year: "2018", result: "L"}
+  })
+  if (won_year) {
+    return won_year.year
+  }
+  else {
+    return won_year
+  }
+}
